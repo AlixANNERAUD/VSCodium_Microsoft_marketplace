b0af485b5a468e94cba77d96e6e17800bdbb671d
Alix ANNERAUD <alix@anneraud.fr>
2024-07-10 11:50:00 -0400

    Translate comments in script in english

--- Install.sh
+++ Install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Sauvegarde du fichier original
+# Backup the original product.json file
 cp /usr/share/codium/resources/app/product.json /usr/share/codium/resources/app/product.json.backup
 
-# Lecture du fichier et remplacement des lignes
+# Replace the serviceUrl, itemUrl, cacheUrl, controlUrl, and recommendationsUrl with the VS Code Marketplace URLs
 cat /usr/share/codium/resources/app/product.json.backup | \
   sed 's|"serviceUrl": "https://open-vsx.org/vscode/gallery",|"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",|' | \
   sed 's|"itemUrl": "https://open-vsx.org/vscode/item"|"itemUrl": "https://marketplace.visualstudio.com/items",\n\t\t"cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",\n\t\t"controlUrl": "",\n\t\t"recommendationsUrl": ""|' > /usr/share/codium/resources/app/product.json
